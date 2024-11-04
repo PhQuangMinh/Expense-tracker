c8585b3ecb3f3aef5fd7cf2adf3e737f963e04e7
PhQuangMinh <phamquangminh15012004@gmail.com>
2024-11-05 00:06:45 +0700

    feature: them dang nhap, dang ki

--- projectassignment/src/main/java/module-info.java
+++ projectassignment/src/main/java/module-info.java
@@ -11,12 +11,22 @@ module org.example.projectassignment {
     requires com.almasb.fxgl.all;
     requires java.desktop;
     requires annotations;
+    requires javafx.graphics;
+    requires com.google.auth.oauth2;
+    requires firebase.admin;
+    requires com.google.auth;
 
     opens org.example.projectassignment to javafx.fxml;
     opens org.example.projectassignment.view to javafx.fxml;
     opens org.example.projectassignment.controller to javafx.fxml;
+    opens org.example.projectassignment.view.auth.signin to javafx.fxml;
+    opens org.example.projectassignment.view.auth.signup to javafx.fxml;
+    opens org.example.projectassignment.model to firebase.admin;
 
     exports org.example.projectassignment;
     exports org.example.projectassignment.view;
     exports org.example.projectassignment.controller;
+    exports org.example.projectassignment.model;
+    exports org.example.projectassignment.view.auth.signin;
+    exports org.example.projectassignment.view.auth.signup;
 }
--- projectassignment/src/main/java/org/example/projectassignment/Main.java
+++ projectassignment/src/main/java/org/example/projectassignment/Main.java
@@ -1,21 +1,16 @@
 package org.example.projectassignment;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
+
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(Main.class.getResource("view/FeatureSelection.fxml"));
-        Scene scene = new Scene(root ,  600, 750);
-        scene.getStylesheets().add(getClass().getResource("view/style.css").toExternalForm());
-        primaryStage.setTitle("Sổ thu chi");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        InitApp initApp = new InitApp();
+        initApp.init(primaryStage);
     }
     public static void main(String[] args){
         launch(args);
