f33edeb5de14edbd223fce5496b1ed61a8fbc695
Hocnv0204 <nguyenhh1102@gmail.com>
2024-11-09 21:50:23 +0700

    Edit password

--- projectassignment/src/main/java/org/example/projectassignment/view/feature/other/OtherScene.java
+++ projectassignment/src/main/java/org/example/projectassignment/view/feature/other/OtherScene.java
@@ -8,14 +8,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.example.projectassignment.Main;
+import org.example.projectassignment.model.User;
+import org.example.projectassignment.view.EditPassword;
+import org.example.projectassignment.view.feature.other.annualreport.AnnualReportScene;
 
 import java.io.IOException;
 
 public class OtherScene {
+    private User user ;
+    public void init(User user){
+        this.user = user ;
+    }
     @FXML
     private void onActionButtonAnnualReport(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/feature/other/annualreport/AnnualReportScene.fxml"));
         Parent root = loader.load();
+        AnnualReportScene annualReportScene = loader.getController();
+        annualReportScene.init(user) ;
+        Scene scene = new Scene(root);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(scene);
+    }
+    @FXML
+    private void onActionButtonEditPassword(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/feature/other/editpassword/EditPassword.fxml"));
+        Parent root = loader.load();
+        EditPassword editPassword = loader.getController();
+        editPassword.init(user) ;
         Scene scene = new Scene(root);
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         currentStage.setScene(scene);
